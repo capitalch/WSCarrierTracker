982919846ca17e52b73034009a4538214e81cc03
SushantAgrawal <sagarwal@netwoven.com>
2018-06-07 11:08:23 +0530

    init

--- demo/Sush/adam1/index.js
+++ demo/Sush/adam1/index.js
@@ -6,6 +6,7 @@ const db = require('./artifacts/db');
 const workbench = require('./artifacts/workbench');
 const app = require('./artifacts/app');
 const util = require('./artifacts/util');
+const numCPUs = require('os').cpus().length;
 
 ibuki.filterOn('initialize').subscribe(d => {
     ibuki.emit('test-seq-promises-start:index:workbench');
