1f8f84b59ef893ec8d596193ea21528b3a9a0e73
capitalch <capitalch@gmail.com>
2018-06-14 19:35:11 +0530

    init

--- demo/Sush/adam1/artifacts/db1.js
+++ demo/Sush/adam1/artifacts/db1.js
@@ -1,65 +1,39 @@
 'use strict';
 const sql = require('mssql');
+// const _ = require('lodash');
 const rx = require('rxjs');
 const operators = require('rxjs/operators');
 const ibuki = require('./ibuki');
 const config = require('../config');
 const dbConfig = config.dbConfig;
 const pool = new sql.ConnectionPool(dbConfig);
+let reqs = [];
 let req1 = null,
     req2 = null;
 
-function createRequest() {
-    const req = new sql.Request(pool);
-}
-
-function queryRequest(req) {
-    req.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
-        // ... error checks
-        // console.log(result.recordset[0].number) // return 1
-        config.prepared.next(1);
-    })
-}
-
 function disburse() {
-    if(req1.pending){
-        if(req2.pending){
-            //wait until somebody is available
-            let sub1 = rx.interval(30).subscribe(d=>{
-                if((!req1.pending) || (!req2.pending)){
-                    ibuki.emit('sql1-update:util>db1');
-                    sub1.unsubscribe();
-                }
-            })
-        } else {
-            req2.pending = true;
-            req2.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
-                req2.pending = false;
-                if (err) {
-                    console.log('req2:', err);
-                } else {                    
-                    // config.prepared.next(1);
-                    console.log('req 2');
-                }
-    
-            })
-        }
-    } else{
-        req1.pending = true;
-        req1.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
-            req1.pending = false;
+    const req = reqs.find((e) => e.isAvailable);
+    if (req) {
+        req.isAvailable = false;
+        req.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
+            req.isAvailable = true;
             if (err) {
-                console.log('req1:', err);
+                console.log(err);
             } else {
-                req1.pending = false;
-                // config.prepared.next(1);
-                console.log('req 1');
+                console.log('req fulfilled: ', req.index);
             }
-        });
+        })
+    } else {
+        let sub1 = rx.interval(30).subscribe(d => {
+            if (reqs.some(e => e.isAvailable)) {
+                ibuki.emit('sql1-update:util>db1');
+                sub1.unsubscribe();
+            };
+        })
     }
 }
 
-ibuki.filterOn('sql1-update:util>db1').subscribe(d=>{
+ibuki.filterOn('sql1-update:util>db1').subscribe(d => {
     disburse();
 });
 
@@ -70,38 +44,49 @@ ibuki.filterOn('sql1-init:index:db').subscribe(
                 console.log('1:', err)
             } else {
                 ibuki.emit('serial-process:index:workbench');
-                req1 = new sql.Request(pool);
-                req2 = new sql.Request(pool);
-                // config.zip.subscribe(d => {
-                //     // queryRequest(req)
-                //     disburse();
-                // });
+                for (let i = 0; i < 10; i++) {
+                    const req = new sql.Request(pool);
+                    req.isAvailable = true;
+                    req.index = i;
+                    reqs.push(req);
+                }
             }
         })
     });
 
-    
-    // if (req1.pending) {
-    //     req2.pending = true;
-    //     req2.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
-    //         if (err) {
-    //             console.log('req2:', err);
-    //         } else {
-    //             req2.pending = false;
-    //             // config.prepared.next(1);
-    //             console.log('req 2');
-    //         }
 
-    //     })
-    // } else {
-    //     req1.pending = true;
-    //     req1.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
-    //         if (err) {
-    //             console.log('req1:', err);
-    //         } else {
-    //             req1.pending = false;
-    //             // config.prepared.next(1);
-    //             console.log('req 1');
-    //         }
-    //     })
-    // }
+// if (req1.pending) {
+//     req2.pending = true;
+//     req2.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
+//         if (err) {
+//             console.log('req2:', err);
+//         } else {
+//             req2.pending = false;
+//             // config.prepared.next(1);
+//             console.log('req 2');
+//         }
+
+//     })
+// } else {
+//     req1.pending = true;
+//     req1.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
+//         if (err) {
+//             console.log('req1:', err);
+//         } else {
+//             req1.pending = false;
+//             // config.prepared.next(1);
+//             console.log('req 1');
+//         }
+//     })
+// }
+// function createRequest() {
+//     const req = new sql.Request(pool);
+// }
+
+// function queryRequest(req) {
+//     req.query('update product set UnitPrice = UnitPrice+1', (err, result) => {
+//         // ... error checks
+//         // console.log(result.recordset[0].number) // return 1
+//         config.prepared.next(1);
+//     })
+// }
