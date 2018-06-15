d3ab660aa4db4073b64c6fc98d6989af5a8a8106
Sabyasachi Bal <sbal@netwovenindia.com>
2018-06-15 18:20:57 +0530

    update code

---
+++ demo/Saby/adam1-sabya/artifacts/logger.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var Logger = {};
+var infoStream = fs.createWriteStream('logs/info.txt');
+var errorStream = fs.createWriteStream('logs/error.txt');
+var xmlStream = fs.createWriteStream('logs/xml.txt');
+
+Logger.info = function (msg) {
+    var message = "\n"+new Date().toISOString() + " : " + msg + "\n";
+    infoStream.write(message);
+
+};
+Logger.logXML = function (msg) {
+    var message = "\n"+new Date().toISOString() + " : " + msg + "\n";
+    xmlStream.write(message);
+
+};
+Logger.error = function (msg) {
+    var message = "\n"+new Date().toISOString() + " : " + msg + "\n";
+    errorStream.write(message);
+};
+
+module.exports = Logger;
