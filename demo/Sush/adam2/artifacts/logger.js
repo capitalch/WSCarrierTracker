4770bd6817257686758be9c32530893743add42b
capitalch <capitalch@gmail.com>
2018-07-07 21:22:10 +0530

    init

--- demo/Sush/adam2/artifacts/logger.js
+++ demo/Sush/adam2/artifacts/logger.js
@@ -1,12 +1,30 @@
 'use strict';
 const winston = require('winston');
+const moment = require('moment');
 
+const options = {
+    file: {
+        level: 'info',
+        // filename: './logs/app.log',
+        filename: './logs/' + moment().format('YYYY-MM-DD-HHMMSS') + '.log',
+        handleExceptions: true,
+        json: true,
+        //   maxsize: 5242880, // 5MB
+        // maxsize: 5120,
+        // maxFiles: 5,
+        colorize: false
+    },
+    console: {
+        level: 'info',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+    }
+};
 const logger = winston.createLogger({
     transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({
-            filename: 'combined.log'
-        })
+        new winston.transports.Console(options.console),
+        new winston.transports.File(options.file)
     ]
 });
 
